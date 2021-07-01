369233ad2222ab2e7a2565f4d60196f7643834a9
Erick-EEV <EEVERICKEEV@gmail.com>
2021-07-01 10:14:58 -0400

    Class and Function built as well as prompts for user

--- SalesTaxJava.java
+++ SalesTaxJava.java
@@ -1,5 +1,15 @@
 // Create a program to calculate the total amount due for an item purchased at a store in Northern Virginia.
 // The total amount due is the cost of the item plus the sales tax.
-// A user should be able to enterthe cost of the item.
+// A user should be able to enter the cost of the item.
 // The program should apply a 5 percent sales tax to the item.
 // Then the program should display to the user the calculated sales tax along with the total cost including the sales tax.
+import java.util.Scanner;
+
+public class SalesTaxJava {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        // final double salesTax = 5%
+        System.out.println("This program will calculate the cost of an item in Nova");
+        System.out.println("How much is the item?");
+    }
+}
