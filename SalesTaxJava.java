d23697c56361bf1f9a79133f21330aad8834bc47
Erick-EEV <EEVERICKEEV@gmail.com>
2021-07-01 10:05:39 -0400

    Instructions

---
+++ SalesTaxJava.java
@@ -0,0 +1,5 @@
+// Create a program to calculate the total amount due for an item purchased at a store in Northern Virginia.
+// The total amount due is the cost of the item plus the sales tax.
+// A user should be able to enterthe cost of the item.
+// The program should apply a 5 percent sales tax to the item.
+// Then the program should display to the user the calculated sales tax along with the total cost including the sales tax.
